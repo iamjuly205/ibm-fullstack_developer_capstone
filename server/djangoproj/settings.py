8a5b92340e8059d7ba3f556ac2922041020067b5
Anh Duy <nguyenlenhduy9412@gmail.com>
2026-01-18 04:41:23 +0700

    Fix ALLOWED_HOSTS for Render deployment

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,14 +30,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
+    '127.0.0.1',
+    'ibm-fullstack-developer-capstone.onrender.com',
     'https://nelsonmsk710-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
+    'https://ibm-fullstack-developer-capstone.onrender.com',
     'https://nelsonmsk710-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai',
     'https://nelsonmsk710-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
     'proxy.cognitiveclass.ai'
+]
 ]
 
 REST_FRAMEWORK = {
